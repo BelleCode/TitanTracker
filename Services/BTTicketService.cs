3ec8788c0b0c9bc28a6da27d03aadbbcc763a2c6
BelleCode <Belinda.tiberio@gmail.com>
2021-08-26 15:13:48 -0700

    Added: Interface - IBTTicketService Service - BTTicketService
    Updated:
    - AddNewTicketAsync
    - ArchiveTicketAsync
    - UpdateTicketAsync

---
+++ Services/BTTicketService.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TitanTracker.Services.Interfaces;
+using TitanTracker.Models;
+using TitanTracker.Data;
+using Microsoft.EntityFrameworkCore;
+using TitanTracker.Models.Enums;
+
+namespace TitanTracker.Services
+{
+    public class BTTicketService : IBTTicketService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IBTRolesService _rolesService;
+        private readonly IBTProjectService projectService;
+
+        public BTTicketService(ApplicationDbContext context, IBTRolesService rolesService, IBTProjectService projectService)
+        {
+            _context = context;
+            _rolesService = rolesService;
+            this.projectService = projectService;
+        }
+
+        // CRUD : Create
+        public async Task AddNewTicketAsync(Ticket ticket)
+        {
+            try
+            {
+                await _context.AddAsync(ticket);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // CRUD : Delete
+        public async Task ArchiveTicketAsync(Ticket ticket)
+        {
+            try
+            {
+                ticket.Archived = true;
+                await UpdateTicketAsync(ticket);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //TODO: AssignTicketAsync
+        public async Task AssignTicketAsync(int ticketId, string userId)
+        {
+        }
+
+        //TODO: GetAllTicketsByCompanyAsync
+        public Task<List<Ticket>> GetAllTicketsByCompanyAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByPriorityAsync(int companyId, string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByStatusAsync(int companyId, string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByTypeAsync(int companyId, string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetArchivedTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByPriorityAsync(string priorityName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByRoleAsync(string role, string userId, int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByStatusAsync(string statusName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByTypeAsync(string typeName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Ticket> GetTicketByIdAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTUser> GetTicketDeveloperAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetTicketsByRoleAsync(string role, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetTicketsByUserIdAsync(string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketPriorityIdAsync(string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketStatusIdAsync(string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketTypeIdAsync(string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CRUD : Update
+        public async Task UpdateTicketAsync(Ticket ticket)
+        {
+            try
+            {
+                _context.Update(ticket);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}

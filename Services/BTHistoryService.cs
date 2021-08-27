37a5b6515202ca0398f9955ecda4cfb7b6c1d381
BelleCode <Belinda.tiberio@gmail.com>
2021-08-27 12:08:07 -0700

    Implemented Interface: (Classes not updated) - BTNotificationService - BTFileService - BTInviteService - BTHistoryService

---
+++ Services/BTHistoryService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TitanTracker.Models;
+using TitanTracker.Services.Interfaces;
+
+namespace TitanTracker.Services
+{
+    public class BTHistoryService : IBTHistoryService
+    {
+        public Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/BTNotificationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TitanTracker.Models;
+using TitanTracker.Services.Interfaces;
+
+namespace TitanTracker.Services
+{
+    public class BTNotificationService : IBTNotificationService
+    {
+        public Task AddNotificationAsync(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

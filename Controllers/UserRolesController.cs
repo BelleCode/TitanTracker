af9f3db5f8479ee514ced02d2ccd41b573ca2023
BelleCode <Belinda.tiberio@gmail.com>
2021-08-31 11:51:17 -0700

    Added: - UserRolesControllers.cs - ManageUserRolesViewModel - ViewModels Folder

---
+++ Controllers/UserRolesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TitanTracker.Extensions;
+using TitanTracker.Models;
+using TitanTracker.Models.ViewModels;
+using TitanTracker.Services.Interfaces;
+
+namespace TitanTracker.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly IBTCompanyInfoService _companyInfoService;
+        private readonly IBTRolesService _rolesService;
+        private readonly int _companyId;
+
+        public UserRolesController(IBTCompanyInfoService companyInfoService,
+                                   IBTRolesService rolesService,
+                                   IHttpContextAccessor contextAccessor)
+        {
+            _companyInfoService = companyInfoService;
+            _rolesService = rolesService;
+
+            if (contextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                _companyId = contextAccessor.HttpContext.User.Identity.GetCompanyId().Value;
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageUserRoles()
+        {
+            List<ManageUserRolesViewModel> model = new();
+            List<BTUser> users = await _companyInfoService.GetAllMembersAsync(_companyId);
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
+        {
+            return View();
+        }
+    }
+}

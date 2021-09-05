7a3570eaef70879b35f379364ff3d76d3712140f
BelleCode <Belinda.tiberio@gmail.com>
2021-09-04 23:15:11 -0700

    Updated: - Details - MyProjects

--- Models/ViewModels/ManageUserRolesViewModel.cs
+++ Models/ViewModels/ManageUserRolesViewModel.cs
@@ -8,8 +8,8 @@ namespace TitanTracker.Models.ViewModels
 {
     public class ManageUserRolesViewModel
     {
-        public BTUser BTUser { get; set; }
-        public MultiSelectList Roles { get; set; }
-        public List<string> SelectedRoles { get; set; }
+        public BTUser BTUser { get; set; }                  //BTUser Model (pre-existing)
+        public MultiSelectList Roles { get; set; }          //Enum (pre-existing)
+        public List<string> SelectedRoles { get; set; }     //List of roles being changed
     }
 }
